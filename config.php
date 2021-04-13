6d4240291440b8267fdc577fbd2fdad38ce50f05
NabidHasan <68748860+NabidHasan@users.noreply.github.com>
2021-04-13 14:21:11 +0600

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$databaseHost = 'localhost';
-$databaseName = 'crud_web';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
-?>
